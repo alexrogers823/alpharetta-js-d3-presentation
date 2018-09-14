89f4a9cfd1b3b5f92c096b39208ade13d69508c9
Alex Rogers <alex.rogers823@gmail.com>
2018-09-14 17:32:12 -0400

    Files for practicing D3.js

---
+++ barGraph-FINISHED.js
@@ -0,0 +1,311 @@
+const dataset = [];
+console.time("Getting data");
+// fetch('https://gist.githubusercontent.com/alexrogers823/00de045f0858cc42e490464349824022/raw/934a543b52060e07008e799247910f9d8c91a544/expenses.json')
+fetch('https://raw.githubusercontent.com/alexrogers823/interactiveBarGraph/master/InteractiveBarGraph/month_expenses_2016.json')
+  .then(blob => blob.json())
+  .then(data => {
+    console.timeEnd("Getting data");
+    dataset.push(...data);
+  });
+
+const test = [];
+fetch('https://raw.githubusercontent.com/jongrim/flashcard-generator/master/firebase.json')
+  .then(blob => blob.json())
+  .then(data => {
+    test.push(...data);
+    console.log(test);
+  })
+
+// Category labels. Find a way to add directly from month expenses data
+const categoryLabels = {
+  Rent: {
+    labels: ["Rent"],
+    goal: 800,
+    color: [209, 145, 105]
+  },
+  Utilities: {
+    labels: ["Util"],
+    goal: 80,
+    color: [165, 20, 165]
+  },
+  Phone: {
+    labels: ["Phone"],
+    goal: 65,
+    color: [70, 150, 215]
+  },
+  Apparel: {
+    labels: ["Clothes"],
+    goal: 50,
+    color: [195, 195, 30]
+  },
+  Supplies: {
+    labels: ["Supplies", "Rain"],
+    goal: 50,
+    color: [42, 193, 243]
+  },
+  Technology: {
+    labels: ["Tech"],
+    goal: 40,
+    color: [30, 240, 100]
+  },
+  Services: {
+    labels: ["Dry Cleaning"],
+    goal: 30,
+    color: [141, 155, 20]
+  },
+  "Health/Gym": {
+    labels: ["Health", "Gym"],
+    goal: 50,
+    color: [164, 72, 235]
+  },
+  Haircut: {
+    labels: ["Haircut"],
+    goal: 40,
+    color: [99, 196, 126]
+  },
+  Groceries: {
+    labels: ["Groceries"],
+    goal: 200,
+    color: [143, 15, 166]
+  },
+  "Other Food": {
+    labels: ["Res", "Snacks"],
+    goal: 300,
+    color: [15, 242, 28]
+  },
+  "Gas & Parking": {
+    labels: ["Gas", "Parking"],
+    goal: 30,
+    color: [34, 38, 141]
+  },
+  Insurance: {
+    labels: ["Insurance"],
+    goal: 40,
+    color: [188, 159, 198]
+  },
+  "Bank & Credit Card": {
+    labels: ["Credit Card", "Interest", "Atm"],
+    goal: 40,
+    color: [62, 141, 138]
+  },
+  "Student Loans": {
+    labels: ["Student Loans"],
+    goal: 400,
+    color: [206, 117, 92]
+  },
+  "Vehicle Payments": {
+    labels: ["Vehicle Payments"],
+    goal: 20,
+    color: [115, 73, 238]
+  },
+  "Entertainment": {
+    labels: ["Movies"],
+    goal: 30,
+    color: [206, 18, 160]
+  },
+  "Subscriptions": {
+    labels: ["Subscription", "Spotify"],
+    goal: 25,
+    color: [124, 232, 134]
+  },
+  Travel: {
+    labels: ["Travel"],
+    goal: 200,
+    color: [118, 55, 65]
+  },
+  "Public Transportation": {
+    labels: ["Uber", "Marta"],
+    goal: 25,
+    color: [252, 158, 155]
+  },
+  "Special/Seasonal": {
+    labels: ["Graduation"],
+    goal: null,
+    color: [227, 250, 194]
+  },
+  Other: {
+    labels: ["Other"],
+    goal: null,
+    color: [100, 100, 100]
+  }
+}
+
+// Scales and Axis (eventually use band scale for y-axis)
+const width = 1100;
+const height = 500;
+const categoryLength = Object.keys(categoryLabels).length;
+const moveX = 40;
+const xDistance = (width - moveX)/categoryLength;
+const barWidth = 30;
+
+const heightScale = d3.scale.linear()
+  .domain([0, 1050])
+  .range([0, height]);
+
+const reverseHeightScale = d3.scale.linear()
+  .domain([0, 1050])
+  .range([height, 0]);
+
+// const categoryScale = d3.scale.ordinal()
+//   .domain(d3.range(categoryLength))
+//   .rangeRoundBands([0, width], .1);
+
+const categoryScale = d3.scale.linear()
+  .domain([0, categoryLength])
+  .range([moveX, width]); //moveX + yAxis distance
+
+const axis = d3.svg.axis()
+  .ticks(20)
+  .tickSize(5)
+  .tickPadding(5)
+  .scale(reverseHeightScale)
+  .orient("left");
+
+const categoryAxis = d3.svg.axis()
+  .ticks(categoryLength)
+  .tickSize(2)
+  .tickPadding(45)
+  .scale(categoryScale)
+  .tickFormat((d, i) => {
+    categoryNames = [];
+    for (let prop in categoryLabels) {
+      categoryNames.push(prop);
+    }
+    return categoryNames[i];
+  })
+  .orient("bottom");
+
+// set variable for smooth transition
+const t = d3.transition().duration(750);
+
+
+// Without d3.json import
+const svgContainer = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height+50)
+.append("g")
+.attr("transform", `translate(${moveX}, -10)`);
+// .call(axis);
+
+const marginSpace = 10;
+
+
+function processData(data, number) {
+  let obj = data[0][`month${number}`].Expenses;
+  let newDataSet = [];
+  // console.log(obj);
+  let totalValue = 0, totalExpenses = 0;
+
+  for (let prop in categoryLabels) {
+    let value = 0;
+    for (let i = 0; i < obj.length; i++) {
+      // Using only one property so that totalExpenses variable is done correctly
+      if (prop === "Rent") {
+        totalExpenses += obj[i].Cost;
+      }
+      if (categoryLabels[prop] !== "Other" && (categoryLabels[prop].labels.includes(obj[i].Category) || obj[i].Category === prop)) {
+        value += obj[i].Cost;
+        totalValue += obj[i].Cost;
+      }
+    }
+    newDataSet.push({Expense: prop, Cost: value, Goal: categoryLabels[prop].goal, Color: categoryLabels[prop].color});
+  }
+
+
+  newDataSet[newDataSet.length-1] = {Expense: "Other", Cost: totalExpenses - totalValue, Goal: categoryLabels["Other"].goal, Color: categoryLabels["Other"].color};
+
+  // console.log(newDataSet)
+  return newDataSet;
+}
+
+// function to set colors for bars
+function colorBars(ht, goal, colors, i) {
+  if (goal && ht > goal) {
+    return 'rgb(200, 0, 0)';
+  }
+  let shade = (goal) ? ht/goal : 1;
+  let red = colors[0], green = colors[1], blue = colors[2];
+  return `rgb(${red*shade}, ${green*shade}, ${blue*shade})`;
+}
+
+
+// Set function to update with new data
+function update(data, number) {
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+  // Rectangles using data
+  const otherRectangles = svgContainer.selectAll(".bar")
+  .data(processData(data, number));
+
+  // Updating month title
+  document.querySelector("span").textContent = `${monthNames[number-1]}`;
+
+  // Updating old elements that are present in new data
+  otherRectangles.transition(t)
+  .attr("x", (d, i) => i * xDistance)
+  .attr("y", (d, i) => height - heightScale(d.Cost) + 1)
+  .attr("width", barWidth)
+  .attr("height", (d, i) => heightScale(d.Cost) + 1)
+  .attr("fill", (d, i) => colorBars(d.Cost, d.Goal, d.Color, i));
+
+  // Creating bars
+  otherRectangles.enter()
+  .append("rect")
+  .attr("class", "bar")
+  .attr("x", (d, i) => i * xDistance)
+  .attr("y", (d, i) => height - heightScale(d.Cost) + 1)
+  .attr("width", barWidth)
+  .attr("height", 0)
+  .attr("fill-opacity", 0.1)
+  .transition(t)
+  .attr("fill-opacity", 1)
+  .attr("fill", (d, i) => colorBars(d.Cost, d.Goal, d.Color, i))
+  .attr("height", (d, i) => heightScale(d.Cost) + 1);
+
+  // Creating goal lines
+  otherRectangles.enter()
+  .append("line")
+  .attr("class", "goalLine")
+  .attr("x1", (d, i) => i * xDistance)
+  .attr("y1", d => (d.Goal) ? height - heightScale(d.Goal) : null)
+  .attr("x2", (d, i) => (i * xDistance) + barWidth)
+  .attr("y2", d => (d.Goal) ? height - heightScale(d.Goal) : null)
+  .attr("stroke-dasharray", 3.2)
+  .attr("stroke", d => (d.Cost > d.Goal) ? "white" : "black")
+  .attr("opacity", 0.4);
+
+}
+
+
+// Calling the axis
+svgContainer.append("g")
+  .attr("class", "yAxis")
+  .attr("transform", `translate(-10, 3)`)
+  .call(axis);
+
+
+// Calling the categorical axis
+svgContainer.append("g")
+  .attr("class", "categoryAxis")
+  .attr("transform", `translate(${moveX/2}, ${height+5})`)
+  .call(categoryAxis)
+  .selectAll("text")
+  .attr("transform", "rotate(90)");
+
+
+// Changing the data each second
+let flag = true, numeric = 1;
+setInterval(() => {
+  update(dataset, numeric);
+  flag = !flag;
+  numeric++;
+  if (numeric > 2) {
+    numeric = 1;
+  }
+
+}, 1500);
+
+// console.log(example);
+// console.log(data1);
+// update(dataset, 1);
